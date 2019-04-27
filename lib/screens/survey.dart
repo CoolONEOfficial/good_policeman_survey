dbf500ee1c41466839eda85d86537357559d480f
coolone <coolone.official@gmail.com>
2019-04-27 12:39:40 +0300

    Improved all

--- lib/screens/survey.dart
+++ lib/screens/survey.dart
@@ -1,13 +1,16 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/widgets.dart' as prefix0;
 import 'package:good_policeman_survey/main.dart';
 import 'package:progress_hud/progress_hud.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:random_string/random_string.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:flutter_tagging/flutter_tagging.dart';
 
 enum Gender {
   Unknown,
@@ -58,6 +61,7 @@ class SurveyModel {
   final Gender gender;
   final String age;
   final bool adContact;
+  final List problems;
 
   const SurveyModel({
     this.areaName,
@@ -66,6 +70,7 @@ class SurveyModel {
     this.gender,
     this.age,
     this.adContact,
+    this.problems,
   });
 
   toJson() => {
@@ -78,12 +83,18 @@ class SurveyModel {
         "gender": _genderNames[gender.index],
         "age": age,
         "adContact": adContact,
+        "problems": problems,
       };
 }
 
-class SurveyScreen extends StatelessWidget {
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
-  final _formKey = GlobalKey<FormState>();
+class SurveyScreen extends StatefulWidget {
+  @override
+  _SurveyScreenState createState() => _SurveyScreenState();
+}
+
+class _SurveyScreenState extends State<SurveyScreen> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>(),
+      _formKey = GlobalKey<FormState>();
 
   final ProgressHUD _progress = ProgressHUD(
     loading: false,
@@ -93,11 +104,14 @@ class SurveyScreen extends StatelessWidget {
     borderRadius: 5.0,
     text: 'Загрузка...',
   );
+
   File _selfieImage;
   Area _area = Area.None;
   Gender _gender = Gender.Unknown;
   double _ageId = 0;
   bool _adContact = false;
+  List _problems = [];
+  String _problemError;
 
   bool _validateAndSave() {
     final form = _formKey.currentState;
@@ -109,15 +123,31 @@ class SurveyScreen extends StatelessWidget {
   }
 
   void _validateAndSubmit(BuildContext ctx) async {
-    if (_validateAndSave()) {
+    bool valid = true;
+
+    if (_problems.isEmpty) {
+      valid = false;
+
+      setState(() {
+        _problemError = "Необходимо ввести проблемы";
+      });
+    }
+
+    if (!_validateAndSave()) {
+      valid = false;
+    }
+
+    if (valid) {
       _progress.state.show();
 
-      final url = await (await storageRef
-              .child("survey/" + duid + '/' + randomAlphaNumeric(10))
-              .putFile(_selfieImage)
-              .onComplete)
-          .ref
-          .getDownloadURL();
+      final url = _selfieImage != null
+          ? await (await storageRef
+                  .child("survey/" + duid + '/' + randomAlphaNumeric(10))
+                  .putFile(_selfieImage)
+                  .onComplete)
+              .ref
+              .getDownloadURL()
+          : null;
 
       final position = await Geolocator()
           .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
@@ -129,6 +159,7 @@ class SurveyScreen extends StatelessWidget {
                 selfieUrl: url,
                 position: position,
                 gender: _gender,
+                problems: _problems.map((problem) => problem["name"]).toList(),
               ).toJson(),
             );
 
@@ -264,16 +295,33 @@ class SurveyScreen extends StatelessWidget {
                             _onGender.add(_gender);
                           },
                           child: Container(
-                            foregroundDecoration: _gender == _genders[i]
-                                ? BoxDecoration()
-                                : BoxDecoration(
-                                    color: Colors.grey,
-                                    backgroundBlendMode: BlendMode.saturation,
-                                  ),
-                            child: Image.asset(
-                              'assets/icons/' + _genderNames[i] + '.png',
-                              width: 100,
-                              height: 100,
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  width: 100,
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                        image: AssetImage('assets/icons/' +
+                                            _genderNames[i] +
+                                            '.png'),
+                                      )),
+                                  foregroundDecoration: _gender == _genders[i]
+                                      ? BoxDecoration()
+                                      : BoxDecoration(
+                                          color: Colors.black,
+                                          shape: BoxShape.circle,
+                                          backgroundBlendMode:
+                                              BlendMode.saturation,
+                                        ),
+                                ),
+                                Container(height: 10),
+                                Text(
+                                  ["М", "Не указано", "Ж"][i],
+                                  style: Theme.of(ctx).textTheme.title,
+                                ),
+                              ],
                             ),
                           ),
                         ),
@@ -319,18 +367,75 @@ class SurveyScreen extends StatelessWidget {
       Padding(
         padding: const EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 0.0),
         child: StreamBuilder(
-          stream: _onAdContact.stream,
-          builder: (ctx, _) {
-            return CheckboxListTile(
-              value: _adContact,
-              onChanged: (value) {
-                _adContact = value;
-                _onAdContact.add(_adContact);
+            stream: _onAdContact.stream,
+            builder: (ctx, _) {
+              return CheckboxListTile(
+                value: _adContact,
+                onChanged: (value) {
+                  _adContact = value;
+                  _onAdContact.add(_adContact);
+                },
+                title: Text('Готов контактировать с администрацией'),
+                controlAffinity: ListTileControlAffinity.leading,
+              );
+            }),
+      );
+
+  Widget _buildAddButton() => Container(
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          color: Colors.grey.withOpacity(0.5),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(
+              Icons.add,
+              color: Colors.white,
+              size: 15.0,
+            ),
+            Text(
+              "Добавить проблему",
+              style: TextStyle(color: Colors.white, fontSize: 14.0),
+            ),
+          ],
+        ),
+      );
+
+  Widget _problemsInput(BuildContext ctx) => Padding(
+        padding: const EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            FlutterTagging(
+              textFieldDecoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Дороги, долгострой...",
+                  labelText: "Введите проблемы"),
+              addButtonWidget: _buildAddButton(),
+              deleteIcon: Icon(Icons.cancel, color: Colors.white),
+              suggestionsCallback: (query) => _suggestions
+                  .map((name) =>
+                      {'name': name, 'value': _suggestions.indexOf(name) + 1})
+                  .where((tag) => tag['name'].toLowerCase().contains(query))
+                  .toList()
+                    ..add({'name': query, 'value': 0}),
+              onChanged: (result) {
+                debugPrint("Problems: " + result.toString());
+                setState(() {
+                  _problemError = null;
+                  _problems = result;
+                });
               },
-              title: Text('Готов контактировать с администрацией'),
-              controlAffinity: ListTileControlAffinity.leading,
-            );
-          }
+            ),
+            Container(height: 10),
+            Text(
+              _problemError ?? "",
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.red),
+            ),
+          ],
         ),
       );
 
@@ -359,6 +464,7 @@ class SurveyScreen extends StatelessWidget {
                   _genderInput(ctx),
                   _ageInput(ctx),
                   _areaInput(ctx),
+                  _problemsInput(ctx),
                   _adContactInput(ctx),
                   _selfieInput(ctx),
                 ],
@@ -369,3 +475,10 @@ class SurveyScreen extends StatelessWidget {
         ),
       );
 }
+
+final List _suggestions = [
+  "Дороги",
+  "Долгострой",
+  "Мусор",
+  "Экология",
+];
--- lib/screens/total.dart
+++ lib/screens/total.dart
@@ -10,30 +10,32 @@ class TotalScreen extends StatelessWidget {
         child: Scaffold(
           body: Align(
             alignment: Alignment.center,
-            child: Padding(
-              padding: const EdgeInsets.all(48.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Text(
-                      "Анкета успешно отправлена",
-                      style: Theme.of(ctx).textTheme.display4.merge(TextStyle(fontSize: 70, color: Colors.white)),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Center(
+                    child: Container(
+                      width: 350,
+                      child: Text(
+                        "Анкета успешно отправлена",
+                        style: Theme.of(ctx).textTheme.display4.merge(TextStyle(fontSize: 70, color: Colors.white)),
+                      ),
                     ),
-                    FloatingActionButton(
-                      backgroundColor: Colors.white,
-                      foregroundColor: Colors.blue,
-                      onPressed: () {
-                        Navigator.of(ctx).pushNamedAndRemoveUntil(
-                          '/survey',
-                          (Route<dynamic> route) => false,
-                        );
-                      },
-                      child: Icon(Icons.refresh),
-                    ),
-                  ]),
-            ),
+                  ),
+                  FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.blue,
+                    onPressed: () {
+                      Navigator.of(ctx).pushNamedAndRemoveUntil(
+                        '/survey',
+                        (Route<dynamic> route) => false,
+                      );
+                    },
+                    child: Icon(Icons.refresh),
+                  ),
+                ]),
           ),
         ),
       );
